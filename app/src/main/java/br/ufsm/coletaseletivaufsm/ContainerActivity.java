ce48a4e8a11a3b7b79851259e85b8001b713725b
emilio hoffmann <emiliohoffmann@hotmail.com>
2016-11-18 11:31:13 -0200

    Nova versão

--- app/src/main/java/br/ufsm/coletaseletivaufsm/ContainerActivity.java
+++ app/src/main/java/br/ufsm/coletaseletivaufsm/ContainerActivity.java
@@ -1,48 +1,35 @@
 package br.ufsm.coletaseletivaufsm;
 
-import android.app.DownloadManager;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
-import android.os.AsyncTask;
-import android.os.Build;
-import android.os.Environment;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.squareup.picasso.Picasso;
-
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Date;
 import java.util.Locale;
-
 import br.ufsm.coletaseletivaufsm.containers.Containers;
 
 public class ContainerActivity extends AppCompatActivity {
 
-    private ProgressDialog mProgressDialog;
-    private String downloadUrl = "https://www.iconexperience.com/_img/v_collection_png/256x256/shadow/cargo_container.png";
-    private Bitmap bitmapDownload;
-    private static final String TAG = MainActivity.class.getSimpleName();
 
+    /**
+     * Metodo de inicialização
+     * Seta todas as Views
+     *
+     * Busca o container que é enviado na abertura do Intent
+     *
+     * Seta a ação de click para gerar o alerta e abrir mapa
+     *
+     * @param savedInstanceState Salvar a instância
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,27 +44,28 @@ public class ContainerActivity extends AppCompatActivity {
 
         nomeContainer.setText(containers.getNome());
         descricaoContainer.setText(containers.getDescricao());
-        //fotoContainer.setImageBitmap(getBitmapFromURL(containers.getLinkfoto()));
 
         imageButtonAbrirMapa.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String uri = String.format(Locale.ENGLISH, "geo:%f,%f?z=19&q=<"+containers.getLatitude()+">,<"+containers.getLongitude()+">("+containers.getNome() +")", containers.getLatitude(),containers.getLongitude());
-                alerta("Abrindo mapa", "Deseja abrir o mapa para ver a localização?", uri);
+                showAlert("Abrindo mapa", "Deseja abrir o mapa para ver a localização?", uri);
             }
         });
 
-
-
-
+        /**
+         * Download da foto e atribuição ao ImageView
+         *
+         */
         if(isOnline()) {
             Picasso.with(getApplicationContext()).load(containers.getLinkfoto()).into(fotoContainer);
         }
-        //new DownloadImageTask(fotoContainer).execute(containers.getLinkfoto() );
-        //fotoContainer.setImageBitmap(bitmapDownload);
 
     }
 
-
+    /**
+     * Metodo para verificação de conexão com a internet
+     * @return Esta conectado?
+     */
     public boolean isOnline() {
         ConnectivityManager cm =
                 (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -85,7 +73,14 @@ public class ContainerActivity extends AppCompatActivity {
         return netInfo != null && netInfo.isConnectedOrConnecting();
     }
 
-    public void alerta(String titulo, String mensagem, final String uri) {
+    /**
+     * Metodo para gerar um alerta na tela e abrir o mapa
+     *
+     * @param titulo Titulo do alerta
+     * @param mensagem Texto no alerta
+     * @param uri URI contendo as coordenadas para abrir o mapa
+     */
+    public void showAlert(String titulo, String mensagem, final String uri) {
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
         alert.setTitle(titulo);
         alert.setMessage(mensagem);
@@ -102,6 +97,11 @@ public class ContainerActivity extends AppCompatActivity {
         alert.show();
     }
 
+    /**
+     * Metodo para abrir o mapa em outra tela
+     *
+     * @param geoLocation URI enviada com as coordenadas pelo alerta
+     */
     public void showMap(Uri geoLocation) {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(geoLocation);
@@ -109,43 +109,4 @@ public class ContainerActivity extends AppCompatActivity {
             startActivity(intent);
         }
     }
-
-
-
-
-
-    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-        ImageView bmImage;
-
-        public DownloadImageTask(ImageView bmImage) {
-            this.bmImage = bmImage;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            Log.i("Async-Example", "onPreExecute Called");
-            mProgressDialog = ProgressDialog.show(ContainerActivity.this,
-                    "Wait", "Downloading Image");
-        }
-
-        protected Bitmap doInBackground(String... urls) {
-            String url = urls[0];
-            Bitmap mIcon = null;
-            try {
-                InputStream in = new java.net.URL(url).openStream();
-                mIcon = BitmapFactory.decodeStream(in);
-            } catch (Exception e) {
-                Log.e("Error", e.getMessage());
-                e.printStackTrace();
-            }
-            return mIcon;
-        }
-
-        protected void onPostExecute(Bitmap result) {
-            mProgressDialog.dismiss();
-            bmImage.setImageBitmap(result);
-        }
-    }
-
-
 }
